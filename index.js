95026e24b3ad6d57986833e165611c725cf18c2b
Project_illwa <taja82@naver.com>
2018-01-16 05:21:14 +0900

    h

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var Discordie = require("discordie");
+
+var cheerio = require('cheerio');//스크래핑
+var request = require('request');//파싱
+
+var replaceall = require('replaceall');
+
 var Events = Discordie.Events;
 
 var client = new Discordie();
+
+function getRandomInt(min, max) { //min ~ max 사이의 임의의 정수 반환
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 client.connect({ token: "NDAyMzUxMDYyNTQ5NTI4NTc3.DT3rTA.XrxprKRpJ8W221oTLBqjrcQy9Gc" });
 
@@ -9,7 +19,44 @@ client.Dispatcher.on(Events.GATEWAY_READY, e => {
 console.log("Connected as: " + client.User.username);
 });
 
+
+//var ran = getRandomInt(1,40);
+
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 if (e.message.content == "ping")
 e.message.channel.sendMessage("pong");
+if (e.message.content == "random") {
+	var rooturl = 'http://safebooru.org/';
+	var url = 'http://safebooru.org/index.php?page=post&s=list&tags=kafuu_chino&pid=' + getRandomInt(1,10)*40;  
+	request(url, function(error, response, html){  
+    if (error) {throw error};
+
+   //console.log (html);
+
+    var $ = cheerio.load(html);
+	//url = rooturl + $('span.thumb').find("a").eq(getRandomInt(1,40)).attr("href");
+	var imgurl = "http:" + $('span.thumb').find("img").eq(getRandomInt(1,40)).attr("src");
+    //$('span.thumb').each(function(){
+		
+        
+		
+		/*request(url, function(error, response, html){  
+		if (error) {throw error};
+		$ = cheerio.load(html);
+		url = "http:" + $('img#image').attr("src");
+		console.log("이미지링크 : " + url);
+		e.message.channel.sendMessage(url,false,{image:{url:"http:" + $('img#image').attr("src")}});
+		});
+		*/
+		imgurl = imgurl.replace("thumbnails","images").replace("thumbnail_","");
+		//replaceall("thumbnails","samples",imgurl);
+		console.log("이미지링크 : " + imgurl);
+		//replaceall("thumbnails","samples",imgurl);
+		e.message.channel.sendMessage("",false,{image:{url:imgurl}});
+    //})
+
 });
+	//e.message.channel.sendMessage(ran);
+}
+});
+
